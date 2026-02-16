a55cc49bafa7cc5f7d78e47285543d17bb09fa24
AdithKP2004 <adith@cst.team>
2026-02-16 11:03:00 +0530

    Adding the google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -51,10 +53,24 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="NC6T0M4-d6kx3bi-D_J6F8851kVi8e-pUK2m1NyEqVc" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WRKT9KV5');
+          `}
+        </Script>
+
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${inter.variable} ${robotoMono.variable} antialiased over`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRKT9KV5"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {children}
       </body>
     </html>
